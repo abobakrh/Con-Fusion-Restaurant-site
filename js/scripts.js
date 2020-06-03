352368539900c8d132de91075d2a01796ec8c6c5
Abobakr Hussein Osman <abobakrhussein.sh@gmail.com>
2020-06-03 19:58:57 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$("#mycarousel").carousel({interval:500});
+$("#carouselButton").click(function(){
+
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');
+    }
+
+});
+$("#reserve-button").click(function(){
+$("#form_modal").modal();
+});
+
+$("#login-button").click(function(){
+$("#loginModal").modal();
+});
